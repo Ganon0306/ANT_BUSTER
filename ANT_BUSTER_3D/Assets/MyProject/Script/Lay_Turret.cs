c99e0f5210ea442ad705f9c20df0f9f6fd5ceeaa
Ganon0306 <134994629+Ganon0306@users.noreply.github.com>
2023-07-25 11:50:09 +0900

    마우스 따라다니기

--- ANT_BUSTER_3D/Assets/MyProject/Script/Lay_Turret.cs
+++ ANT_BUSTER_3D/Assets/MyProject/Script/Lay_Turret.cs
@@ -3,69 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Lay_Turret : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class Lay_Turret : MonoBehaviour
 {
-    public GameObject prefabToInstantiate; // 드래그 중에 생성할 프리팹
+    public GameObject prefabToInstantiate; // 마우스를 따라다닐 프리팹 오브젝트
+    public float moveSpeedMultiplier = 10f; // 마우스 움직임에 곱할 값
 
-    public Transform followObject;
+    private GameObject instantiatedObject; // 생성된 오브젝트를 담을 변수
 
-    private RectTransform rectTransform;
-    private Vector2 initialPosition;
-    private bool isDragging = false;
-    private GameObject instantiatedPrefab; // 생성된 프리팹을 담을 변수
-
-    private void Start()
+    public void OnButtonPressed()
     {
-        rectTransform = GetComponent<RectTransform>();
-        initialPosition = rectTransform.position;
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        isDragging = true;
-        Debug.LogFormat("{0}", isDragging);
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if (isDragging)
-        {
-            isDragging = false;
-            Debug.LogFormat("{0}", isDragging);
-            DropLogic(); // 드롭 로직 실행
-        }
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (isDragging)
-        {
-            Vector3 screenPoint = new Vector3(eventData.position.x, eventData.position.y, Camera.main.nearClipPlane);
-            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(screenPoint);
-            instantiatedPrefab.transform.position = worldPoint;
-            // 드래그 중에는 마우스의 움직임에 따라 UI 오브젝트를 이동시킵니다.
-            //Vector3 screenPoint = new Vector3(eventData.position.x, eventData.position.y, Camera.main.nearClipPlane);
-            //Vector3 worldPoint = Camera.main.ScreenToWorldPoint(screenPoint);
-
-            //worldPoint.y = 4;
-
-            //followObject.position = worldPoint;
-        }
-    }
-    private void CreatePrefab()
-    {
-        // 프리팹을 버튼 위치에 생성
+        // 버튼을 누를 때 프리팹을 마우스 위치에 생성
         if (prefabToInstantiate != null)
         {
-            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(rectTransform.position);
-            instantiatedPrefab = Instantiate(prefabToInstantiate, worldPoint, Quaternion.identity);
+            Vector3 screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f); // 적절한 z값 설정
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(screenPoint);
+            worldPoint.y = 1f;
+            worldPoint.x *= 2.3f;
+            worldPoint.z *= 2.3f;
+            instantiatedObject = Instantiate(prefabToInstantiate, worldPoint, Quaternion.identity);
         }
     }
-    private void DropLogic()
+
+    private void Update()
     {
-        // 드롭 로직을 여기에 구현합니다.
-        // 마우스를 떼는 순간 드랍 로직이 실행됩니다.
-        // 드랍 로직에서 원하는 동작을 수행합니다.
-        // 예를 들어, 드롭한 위치를 검사하고, 해당 위치에 맞는 처리를 수행할 수 있습니다.
+        // 생성된 오브젝트를 마우스 위치로 따라다니게 함
+        if (instantiatedObject != null)
+        {
+            Vector3 screenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f); // 적절한 z값 설정
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(screenPoint);
+            worldPoint.y = 1f; 
+            worldPoint.x *= 2.3f;
+            worldPoint.z *= 2.3f;
+
+            Vector3 moveDirection = (worldPoint - instantiatedObject.transform.position).normalized;
+            float distance = Vector3.Distance(worldPoint, instantiatedObject.transform.position);
+            float step = moveSpeedMultiplier * Time.deltaTime;
+
+            if (distance > step)
+            {
+                instantiatedObject.transform.position += moveDirection * step;
+            }
+            else
+            {
+                instantiatedObject.transform.position = worldPoint;
+            }
+        }
     }
 }
+
+//////////////////////////////////////111111111111111111111111111111
